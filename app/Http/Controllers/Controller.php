e356cf66e5d9a6f83e66cdaa0d7beb992c23ae5a
Vinicio Cruz <viniciocra@gmail.com>
2025-07-06 03:16:02 -0600

    index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,10 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function index()
+{
+    $productos = \App\Models\Product::all(); 
+    return view('products.index', compact('productos'));
+}
+
 }
